ce42d5158e8d1ce37de429c30a8235c0115489d6
Liliya Khuzhina <liliya.khuzhina@gmail.com>
2022-08-09 14:18:49 +0300

    created serializers for Comment and Review, created views for Review

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework import viewsets
+
+from reviews.models import Comment, Review, Title
+from .serializers import CommentSerializer, ReviewSerialiser
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerialiser
+
+    def get_title(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        return title
+
+    def get_queryset(self):
+        title = self.get_title()
+        new_queryset = title.reviews.all()
+        return new_queryset
+
+    def perform_create(self, serializer):
+        title = self.get_title()
+        serializer.save(title=title, author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+

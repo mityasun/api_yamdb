3816fef980c3e1a2e17c0af4b451c8f3296a1641
Liliya Khuzhina <liliya.khuzhina@gmail.com>
2022-08-09 14:18:49 +0300

    created serializers for Comment and Review, created views for Review

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
+from reviews.models import Comment, Review
+
+
+class ReviewSerialiser(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username'
+    )
+    title = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Review.objects.all(),
+                fields=('author', 'title')
+            )
+        ]
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username'
+    )
+    title = serializers.PrimaryKeyRelatedField(read_only=True)
+    review = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import include, path
 from django.views.generic import TemplateView
+
 from rest_framework.routers import DefaultRouter
 
+from .views import CommentViewSet, ReviewViewSet
+
+app_name = 'api'
 
 router_v1 = DefaultRouter()
+router_v1.register('reviews', ReviewViewSet)
+router_v1.register('comments', CommentViewSet)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),

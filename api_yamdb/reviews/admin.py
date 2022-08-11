c5666eb7615732311a611ccf010de2631dca7c24
Liliya Khuzhina <liliya.khuzhina@gmail.com>
2022-08-11 19:18:07 +0300

    added Comment and Review to admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -21,8 +21,24 @@ class TitleAdmin(admin.ModelAdmin):
     empty_value_display = '-не указано-'
 
 
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'author', 'title', 'text', 'score', 'pub_date'
+    )
+    search_fields = ('title', 'text')
+    list_filter = ('author', 'title', 'pub_date')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'author', 'title', 'review', 'text', 'pub_date'
+    )
+    search_fields = ('title', 'text', 'review')
+    list_filter = ('author', 'title', 'review', 'pub_date')
+
+
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Title, TitleAdmin)
-admin.site.register(Review)
-admin.site.register(Comment)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)

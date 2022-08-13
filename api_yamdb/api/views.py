f9b3d0d378aaae594e57716744b47a8dab2593f5
Артем Петухов <mityasun@gmail.com>
2022-08-13 20:39:24 +0500

    написал README.md

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,3 @@
-from django_filters import rest_framework as flt
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404

4aac6cace36b1fb39d1d203a8ac06f274d0ebae4
Артем Петухов <mityasun@gmail.com>
2022-08-17 23:30:30 +0500

    Поправил валидатор

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -10,8 +10,9 @@ class ValidateUsername:
         pattern = re.compile(r'^[\w.@+-]+')
 
         if pattern.fullmatch(username) is None:
-            raise ValidationError('Некорректные символы в username!')
+            match = re.split(pattern, username)
+            symbol = ''.join(match)
+            raise ValidationError(f'Некорректные символы в username: {symbol}')
         elif username == 'me':
             raise ValidationError('Ник "me" нельзя регистрировать!')
         return username
-

ea607f65f6d20b4cc86e54705d18b2057ec82842
Артем Петухов <mityasun@gmail.com>
2022-10-27 20:23:14 +0500

    Change settings for Django SECRET_KEY

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('TOKEN', 'default-value')
 
 DEBUG = True
 

c0d9ef0ec184b91230273c501fd6ead9ae708b69
Артем Петухов <mityasun@gmail.com>
2022-11-06 19:11:46 +0500

    Add Dockerfile, docker-compose.yaml, workflow

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,15 +1,30 @@
+# import os
+# from datetime import timedelta
+#
+# from dotenv import load_dotenv
+#
+# load_dotenv(dotenv_path='../infra/.env')
+#
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#
+# SECRET_KEY = os.getenv('SECRET_KEY', default='default-value')
+#
+# DEBUG = os.getenv('DEBUG', default=False)
+#
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['*'])
+
 import os
 from datetime import timedelta
 
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='../infra/.env')
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='default-value')
 
-DEBUG = os.getenv('DEBUG', default=False)
+DEBUG = os.getenv("DEBUG", default=False)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['*'])
 
